f713d2706913a4db1e9df5a81bcb8a736055a225
Kex1016 <nyilasdavid@gmail.com>
2022-10-29 12:59:44 +0200

    Fucky-wucky

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import './lib/setup';
+import {LogLevel, SapphireClient} from '@sapphire/framework';
+import {Intents} from "discord.js";
+
+const intents = new Intents(32767);
+const client = new SapphireClient({
+    defaultPrefix: '!',
+    regexPrefix: /^(hey +)?bot[,! ]/i,
+    caseInsensitiveCommands: true,
+    logger: {
+        level: LogLevel.Debug
+    },
+    shards: 'auto',
+    intents: intents,
+    partials: ['CHANNEL'],
+    loadMessageCommandListeners: true
+});
+
+const main = async () => {
+    try {
+        client.logger.info('Logging in');
+        await client.login();
+        client.logger.info('logged in');
+    } catch (error) {
+        client.logger.fatal(error);
+        client.destroy();
+        process.exit(1);
+    }
+};
+
+main();
